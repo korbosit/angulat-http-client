e6ef24a0748a2defaeac6f23015b32d7fd05c928
Vlaimir Korba <krissky1986@gmail.com>
2024-03-05 13:50:55 +0200

    #138 - Creating Records with HTTP Post Request |

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { Task } from '../Model/Task';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CreateTaskComponent],
+  imports: [CommonModule, CreateTaskComponent, HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   showCreateTaskForm: boolean = false;
+  http: HttpClient = inject(HttpClient);
 
   OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
@@ -22,6 +28,16 @@ export class DashboardComponent {
   }
 
   CreateTask(data: Task) {
-    console.log(data);
+    // console.log(data);
+    const headers = new HttpHeaders({ 'my-header': 'hello-world' });
+    this.http
+      .post<{ name: string }>(
+        'https://angularhttpclient-fc045-default-rtdb.firebaseio.com/tasks.json',
+        data,
+        { headers: headers }
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }
