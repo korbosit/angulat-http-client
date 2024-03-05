35b31518587f14c529a8f2a3dd4f8c099ac47de5
Vlaimir Korba <krissky1986@gmail.com>
2024-03-05 14:39:22 +0200

    #139 - Fetching Data with HTTP GET Request |

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { Task } from '../Model/Task';
@@ -7,6 +7,7 @@ import {
   HttpClientModule,
   HttpHeaders,
 } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,9 +16,13 @@ import {
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   showCreateTaskForm: boolean = false;
   http: HttpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.fetchAllTasks();
+  }
 
   OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
@@ -38,6 +43,38 @@ export class DashboardComponent {
       )
       .subscribe((response) => {
         console.log(response);
+      });
+  }
+
+  /* {
+    key: {},
+    key: {},
+  }
+  */
+
+  private fetchAllTasks() {
+    // This code will return us an array and in this array you will have task objects
+
+    this.http
+      .get<{ [key: string]: Task }>(
+        'https://angularhttpclient-fc045-default-rtdb.firebaseio.com/tasks.json'
+      )
+      .pipe(
+        map((response) => {
+          // We'll get an object, so we'll convert it to an array of objects
+          let tasks = [];
+          for (let key in response) {
+            if (response.hasOwnProperty(key)) {
+              // Push a new object in the array
+              tasks.push({ ...response[key], id: key });
+            }
+          }
+
+          return tasks;
+        })
+      )
+      .subscribe((tasks) => {
+        console.log(tasks);
       });
   }
 }
