8a6b310e177df53d4b1596078aa0f497caf56a24
Vlaimir Korba <krissky1986@gmail.com>
2024-03-06 13:57:22 +0200

    #142 - Using Services for HTTP Request

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ import {
   HttpClientModule,
   HttpHeaders,
 } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { TaskService } from '../Services/task.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,6 +20,7 @@ export class DashboardComponent implements OnInit {
   showCreateTaskForm: boolean = false;
   http: HttpClient = inject(HttpClient);
   allTasks: Task[] = [];
+  taskService: TaskService = inject(TaskService);
 
   ngOnInit() {
     this.fetchAllTasks();
@@ -34,19 +35,7 @@ export class DashboardComponent implements OnInit {
   }
 
   CreateTask(data: Task) {
-    // console.log(data);
-    const headers = new HttpHeaders({ 'my-header': 'hello-world' });
-    this.http
-      .post<{ name: string }>(
-        'https://angularhttpclient-fc045-default-rtdb.firebaseio.com/tasks.json',
-        data,
-        { headers: headers }
-      )
-      .subscribe((response) => {
-        console.log(response);
-        // so that when you click the create task button a new task appeared on the dashboard
-        this.fetchAllTasks();
-      });
+    this.taskService.CreateTask(data);
   }
 
   FetchAllTaskClicked() {
@@ -60,106 +49,16 @@ export class DashboardComponent implements OnInit {
   */
 
   private fetchAllTasks() {
-    // This code will return us an array and in this array you will have task objects
-
-    this.http
-      .get<{ [key: string]: Task }>(
-        'https://angularhttpclient-fc045-default-rtdb.firebaseio.com/tasks.json'
-      )
-      .pipe(
-        map((response) => {
-          // We'll get an object, so we'll convert it to an array of objects
-          let tasks = [];
-          for (let key in response) {
-            if (response.hasOwnProperty(key)) {
-              // Push a new object in the array
-              tasks.push({ ...response[key], id: key });
-            }
-          }
-
-          return tasks;
-        })
-      )
-      .subscribe((tasks) => {
-        // console.log(tasks);
-        this.allTasks = tasks;
-        // console.log(tasks);
-      });
+    this.taskService.GetAllTasks().subscribe((tasks) => {
+      this.allTasks = tasks;
+    });
   }
 
   DeleteTask(id: string | undefined) {
-    this.http
-      .delete(
-        `https://angularhttpclient-fc045-default-rtdb.firebaseio.com/tasks/${id}.json`
-      )
-      // .subscribe((res) => {
-      //   console.log(res);
-      // });
-      .subscribe((res) => {
-        this.fetchAllTasks();
-      });
+    this.taskService.DeleteTask(id);
   }
 
   DeleteAllTask() {
-    this.http
-      .delete(
-        `https://angularhttpclient-fc045-default-rtdb.firebaseio.com/tasks.json`
-      )
-      .subscribe((res) => {
-        this.fetchAllTasks();
-      });
+    this.taskService.DeleteAllTasks();
   }
 }
-
-// Конечно, предположим, у вас есть объект response, который содержит данные о задачах в виде ключей и объектов задач:
-
-// typescript
-// Copy code
-// let response = {
-//   "-NsDNTHvD-o4Gh7xFgA7": {
-//     title: "Task 1",
-//     assignedTo: "John",
-//     priority: "High",
-//     status: "Open"
-//   },
-//   "-NsDNTHvD-o4Gh7xFgA8": {
-//     title: "Task 2",
-//     assignedTo: "Jane",
-//     priority: "Medium",
-//     status: "In Progress"
-//   }
-// };
-// Когда вы применяете ваш цикл for...in к объекту response и используете его для создания массива tasks, результат будет выглядеть следующим образом:
-
-// typescript
-// Copy code
-// let tasks = [];
-
-// for (let key in response) {
-//   if (response.hasOwnProperty(key)) {
-//     tasks.push({ ...response[key], id: key });
-//   }
-// }
-
-// console.log(tasks);
-// После выполнения этого кода массив tasks будет выглядеть так:
-
-// typescript
-// Copy code
-// [
-//   {
-//     title: "Task 1",
-//     assignedTo: "John",
-//     priority: "High",
-//     status: "Open",
-//     id: "-NsDNTHvD-o4Gh7xFgA7"
-//   },
-//   {
-//     title: "Task 2",
-//     assignedTo: "Jane",
-//     priority: "Medium",
-//     status: "In Progress",
-//     id: "-NsDNTHvD-o4Gh7xFgA8"
-//   }
-// ]
-// В этом массиве каждая задача расширена свойством id, которое соответствует ключу объекта response, и все остальные свойства задачи остаются такими же, как в исходных объектах задач в response.
