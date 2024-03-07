f0cb9686a181ac3dc312442bf93c832167e96281
Vlaimir Korba <krissky1986@gmail.com>
2024-03-07 20:23:16 +0200

    #154 - Modify Request & Response

--- src/app/Services/auth-interceptor.sevice.ts
+++ src/app/Services/auth-interceptor.sevice.ts
@@ -1,13 +1,26 @@
 import {
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // if(req.url !== '')
-    console.log('Auth Interceptor called!');
-    return next.handle(req);
+    // console.log('Auth Interceptor called!');
+    const modifiedReq = req.clone({
+      headers: req.headers.append('auth', 'abcxyz'),
+    });
+    // return next.handle(modifiedReq);
+    return next.handle(modifiedReq).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Response has arrived. Response data: ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
